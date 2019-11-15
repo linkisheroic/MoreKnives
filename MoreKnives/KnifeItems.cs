e6c9bac972fc859db48a62fb6062114be6515118
naturaGodhead <linkisheroic@gmail.com>
2019-11-15 10:45:58 -0500

    Initial Commit

---
+++ MoreKnives/KnifeItems.cs
@@ -0,0 +1,47 @@
+﻿using SMLHelper.V2.Assets;
+using SMLHelper.V2.Handlers;
+using UnityEngine;
+
+namespace MoreKnives.Items
+{
+    internal abstract class KnifeItems : Spawnable
+    {
+        public static TechType KnivesID { get; protected set; }
+
+        internal static void PatchBioPlasmaItems()
+        {
+            var stasisKnife = new StasisKnife();
+
+            stasisKnife.Patch();
+        }
+
+        protected abstract TechType BaseType { get; }
+
+        protected KnifeItems(string classID, string friendlyName, string description) : base(classID, friendlyName, description)
+        {
+
+        }
+
+        public override GameObject GetGameObject()
+        {
+            GameObject prefab = CraftData.GetPrefabForTechType(this.BaseType);
+            var obj = GameObject.Instantiate(prefab);
+
+            return obj;
+        }
+    }
+    class StasisKnife : KnifeItems
+    {
+        public StasisKnife()
+            : base(classID: "stasisknife", friendlyName: "Stasis Knife", description: "This upgraded knife implements the functionality of your Stasis Rifle.")
+        {
+            OnFinishedPatching += SetStaticTechType;
+        }
+
+        protected override TechType BaseType { get; } = TechType.HeatBlade;
+
+        public override string AssetsFolder { get; } = @"MoreKnives/Assets";
+
+        private void SetStaticTechType() => KnivesID = this.TechType;
+    }
+}
---
+++ MoreKnives/Main.cs
@@ -0,0 +1,35 @@
+﻿/// <summary>
+/// Big thank you to SeraphimRisen and the rest of the modding discord team for all the help on this project.
+/// </summary>
+
+namespace MoreKnives
+{
+    using System;
+    using System.Reflection;
+    using Harmony;
+    using Items;
+    using Common;
+
+    public static class Main
+    {
+        public const string modName = "[More Knives]";
+
+        public static void Patch()
+        {
+            RoseLogger.PatchStart(modName, "1.0.0");
+            try
+            {
+                var harmony = HarmonyInstance.Create("rose.moreknives.mod");
+                KnifeItems.PatchKnifeItems();
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+                RoseLogger.PatchComplete(modName);
+            }
+            
+            catch(Exception ex)
+            {
+                RoseLogger.PatchFailed(modName);
+            }
+        }
+    }
+}
